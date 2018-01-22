3a63efeda5a759e5d64547657ccf6c757208ff0e
Amy <amy.jeanette.morrson@gmail.com>
2018-01-22 20:09:13 +0000

    word counter model broken sorting code

--- app/src/main/java/com/codeclan/amymorrison/wordcounter/WordCount.java
+++ app/src/main/java/com/codeclan/amymorrison/wordcounter/WordCount.java
@@ -1,6 +1,10 @@
 package com.codeclan.amymorrison.wordcounter;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by amymorrison on 22/01/2018.
@@ -14,7 +18,47 @@ public class WordCount {
         wordCount = new HashMap<>();
     }
 
-    public void buildWordCounter(){
-        wordCount();
+    public void buildWordCounter(String paragraph){
+        String[] words = paragraph.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+      for (int i = 0; i < words.length; i++) {
+          Integer count = this.wordCount.get(words[i]);
+          if (count == null) {
+            this.wordCount.put(words[i], 1);
+          } else {
+              this.wordCount.put(words[i], count+1);
+          }
+      }
+    }
+
+    public HashMap<String,Integer> getWordFrequency() {
+        return this.wordCount;
+    }
+
+    public Map<String, Integer> sortWordFrequency() {
+        //doesnt work
+        Map<String, Integer> wordCounter = getWordFrequency();
+        MyComparator comparator = new MyComparator(wordCounter);
+        Map<String, Integer> newMap = new TreeMap<String, Integer>(comparator);
+        newMap.putAll(wordCounter);
+        return newMap;
+    }
+}
+
+class MyComparator implements Comparator<Object> {
+
+    Map<String, Integer> map;
+
+    public MyComparator(Map<String, Integer> map) {
+        this.map = map;
+    }
+
+    public int compare(Object o1, Object o2) {
+
+        if (map.get(o2) == map.get(o1))
+            return 1;
+        else
+            return ((Integer) map.get(o2)).compareTo((Integer)
+                    map.get(o1));
+
     }
 }
--- app/src/test/java/com/codeclan/amymorrison/wordcounter/WordCountTest.java
+++ app/src/test/java/com/codeclan/amymorrison/wordcounter/WordCountTest.java
@@ -1,5 +1,12 @@
 package com.codeclan.amymorrison.wordcounter;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by amymorrison on 22/01/2018.
  */
@@ -7,5 +14,21 @@ package com.codeclan.amymorrison.wordcounter;
 public class WordCountTest {
 
 
-    WordCount wordCounter =
+    WordCount wordCounter;
+    HashMap<String, Integer> wordFrequency;
+
+    @Before
+    public void before(){
+        wordCounter = new WordCount();
+        wordFrequency = new HashMap<>();
+    }
+
+    @Test
+    public void canCountWordFrequency(){
+        String sampleText = "Why do people get especially passionate about pronunciation, using language that we might think more appropriate as a reaction to a terrorist attack than to an intruded “r” (as in “law(r) and order”)? One reason is that pronunciation isn’t like the other areas of speech which generate complaints, such as vocabulary and grammar. You may not like the way people use a particular word, such as disinterested, but you’re not going to meet that problem frequently. Similarly, if you don’t like split infinitives, you won’t hear one very often. But every word has to be pronounced, so if you don’t like the sound of an accent, or the way someone drops consonants, stresses words, or intones a sentence with a rising inflection, there’s no escape. Pronunciation is always there, in your ears.";
+        wordCounter.buildWordCounter(sampleText);
+        //assertEquals(wordFrequency, wordCounter.getWordFrequency());
+        wordCounter.sortWordFrequency();
+        System.out.println(wordCounter.getWordFrequency());
+    }
 }
